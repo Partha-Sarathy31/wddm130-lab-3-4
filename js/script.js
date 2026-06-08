ddf2b76f425c22ac442f2820e5b110365b350ddb
Partha-Sarathy31 <parthavasu3182@gmail.com>
2026-06-08 17:19:33 -0400

    Final JS Update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var tabTop =`<table> 
 <thead>
     <tr> 
-    <th>Name </th> <th> Product </th> <th> Qty </th> <th> Sub Total </th> <th> Total </th>
+    <th>Name </th> <th> Product </th> <th> Quantity </th> <th> Sub Total </th> <th> Total </th>
     </tr>
     </thead>
     <tbody>`
@@ -11,6 +11,8 @@ var tabTop =`<table>
 
 function getPrice(type){
   var price = 0;
+
+  type = type.trim().toLowerCase();
 
   if (type == "laptop"){
     price = 1000;
@@ -24,7 +26,7 @@ function getPrice(type){
 }
 
 function calculateDiscount(subTotal, promoCode){
-  if(promoCode = "SAVE20"){
+  if(promoCode == "SAVE20"){
     subTotal -= 20;
   }
 
@@ -49,7 +51,7 @@ function processForm(){
   var subTotal = 0
   var total = 0;
 
-  var errors = ""
+  var errors=""
   var postPattern = /^[a-zA-Z]\d[a-zA-Z]\s\d[a-zA-Z]\d$/
   var phonePattern = /^\d{3}(\s|-)\d{3}(\s|-)\d{4}$/
   var fm = document.forms["custform"];
@@ -62,34 +64,29 @@ function processForm(){
 
 
   if(name.length < 2){
-    errors+=
-    `<h3>Name should be atleast 2 Characters </h3>`
+    errors+=`<h3>Name should be atleast 2 Characters </h3>`
   }
 
   if(qty < 1){
-    errors+=
-    `<h3>Quantity should be atleast 1 </h3>`
+    errors+=`<h3>Quantity should be atleast 1 </h3>`
   }
 
   if(!regTest(postal, postPattern)){
-    errors+= 
-    `<h3>Postal Code is incorrect, the format should be A1B 3X4 </h3>`
+    errors+=`<h3>Postal Code is incorrect, the format should be A1B 3X4 </h3>`
   }
 
   if(!regTest(phone, phonePattern)){
-    errors+= 
-    `<h3>Phone Number is incorrect, the format should be 123 456 7890 </h3>`
+    errors+=`<h3>Phone Number is incorrect, the format should be 123 456 7890 </h3>`
   }
 
   if(errors !=""){
-    errors+=
     document.getElementById("right").innerHTML = errors;
   } else {
     document.getElementById("right").innerHTML = ""
 
     subTotal = getPrice(type) * qty;
-    subTotal = calculateDiscount(subTotal, promo).toFixed;
-    total = claculateFinalTotal(subTotal, 13);
+    subTotal = calculateDiscount(subTotal, promo).toFixed(2);
+    total = claculateFinalTotal(subTotal, 13).toFixed(2);
 
     tabCt = tabCt + `<tr> 
       <td> ${name} </td>
@@ -97,9 +94,9 @@ function processForm(){
       <td> ${qty} </td>
       <td> ${subTotal} </td>
       <td> ${total} </td>
-    </t>`
+    </tr>`
 
-    document.getElementById("right").innerHTML = tabTop + tabCT + tabBot;
+    document.getElementById("right").innerHTML = tabTop + tabCt + tabBot;
   }
 
 
